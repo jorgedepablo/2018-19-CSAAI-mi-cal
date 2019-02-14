186a586d2b6c74b7968a9594c1cbe48bf6b7443d
Jorge De Pablo Martinez <jdepablo@alpha29.aulas.gsyc.urjc.es>
2019-02-14 11:27:24 +0100

    pulso boton

--- cal-js.js
+++ cal-js.js
@@ -1,2 +1,10 @@
 /*Javascript file*/
-console.log('Hola gozon');
+function main() {
+    console.log('Hola gozon');
+    var button1 = document.getElementById("1")
+    button1.onclick = () => {
+       console.log('Hago click');
+       var display = document.getElementById('display');
+       display.innerHTML = '1';
+   }
+}
