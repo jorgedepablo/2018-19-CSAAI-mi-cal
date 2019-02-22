d0eba5a432a0ede96c75d55b977b2141fe07d6fc
Jorge De Pablo MArtinez <jdepablo96@gmail.com>
2019-02-22 11:15:39 +0100

    pequeños avances

--- cal-js.js
+++ cal-js.js
@@ -1,10 +1,20 @@
 /*Javascript file*/
 function main() {
     console.log('Hola gozon');
-    var button1 = document.getElementById("1")
-    button1.onclick = () => {
+    var b1 = document.getElementById('b1')
+    var b2 = document.getElementById('b2')
+    var b3 = document.getElementById('b3')
+    var b4 = document.getElementById('b4')
+    var b5 = document.getElementById('b5')
+    var b6 = document.getElementById('b6')
+    var b7 = document.getElementById('b7')
+    var b8 = document.getElementById('b8')
+    var b9 = document.getElementById('b9')
+    var b0 = document.getElementById('b0')
+
+    b1.onclick = () => {
        console.log('Hago click');
        var display = document.getElementById('display');
-       display.innerHTML = '1';
-   }
+       display.innerHTML += b1.name;
+    }
 }
